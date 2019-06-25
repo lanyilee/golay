213592bb220477a536ef6f706cd8965c139af6cf
lanyi <lanyilee@qq.com>
2019-06-25 18:20:18 +0800

    route1

--- js/JqUtils.js
+++ js/JqUtils.js
@@ -1,4 +1,5 @@
 layui.define(function(exports){
+    var $ = layui.$;
     var obj ={
         //获取当前页面的相对路径前缀
         getCurPageRelativePathPrefix:function () {
@@ -7,6 +8,22 @@ layui.define(function(exports){
             var pos = curWwwPath.indexOf(pathName);
             var localhostPath = curWwwPath.substring(0, pos);
             return localhostPath;
+        },
+        GetHtml:function(url){
+            var returnHtml="";
+            $.ajax({
+                type: "GET",
+                url: url,
+                async:false,
+                dataType: "html",
+                success: function (data, textStatus) {
+                    returnHtml = data;
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    layer.msg("你没有相关权限");
+                }
+            });
+            return returnHtml;
         }
     }
     exports('jqUtils',obj);
